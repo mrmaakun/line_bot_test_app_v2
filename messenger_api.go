05a3e7e2aa166e456c704df465d5e5e432623107
Serrano Mark <JP11686@geipv6test.local>
2016-11-02 12:01:31 +0900

    Added the ability to change the port using an environment variable PORT

--- messenger_api.go
+++ messenger_api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func DefaultPathHandler(http.ResponseWriter, *http.Request) {
@@ -15,13 +16,28 @@ func DefaultPathHandler(http.ResponseWriter, *http.Request) {
 
 func registerRouteHandlers() {
 
+	log.Println("Registering Route Handlers")
+
 	http.HandleFunc("/", DefaultPathHandler)
-	log.Fatal(http.ListenAndServe((":12345"), nil)) //nil means the default Router Server is used
+
+	var endpoint_port string
+	// If port is set an the environment variables, use that
+	if endpoint_port = os.Getenv("PORT"); endpoint_port == "" {
+
+		// Default endpoint is 12345
+		endpoint_port = "12345"
+		log.Println("setting port")
+
+	}
+
+	log.Println("Listening on port " + endpoint_port)
+	log.Fatal(http.ListenAndServe(":"+endpoint_port, nil)) //nil means the default Router Server is used
 
 }
 
 func main() {
 
+	log.Println("V2 Test Bot Started")
 	registerRouteHandlers()
 	log.Println("Registered Route Handlers")
 
